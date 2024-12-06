ce237834dc64ced19d6212fb424dec79a081bd1e
SMILEY-Maseko <wakhekilemaseko@gmail.com>
2024-12-06 15:05:49 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,11 @@ const routes = require('./routes'); // Import routes from routes.js
 const app = express();
 
 app.use(cors({
-    origin: 'blog-space-rouge.vercel.app',
+    origin: 'https://blog-space-rouge.vercel.app', // Include protocol
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify allowed methods if necessary
+    credentials: true, // If cookies or auth headers are used
 }));
+
 
 app.use(express.json()); // Middleware to parse JSON
 app.use('/api', routes); // Use the routes defined in routes.js
